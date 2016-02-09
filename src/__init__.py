9cb0580e738ee33a4c59b2c17c5407b82b8726bb
Dmitriy Yakovlev <zimy@yandex.ru>
2016-02-09 13:19:00 +0300

    Full flow

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
 # inner = sys.stdin.read()
+from gitworks import commit_patch
 from handsome import full_project_info
 from translation import yandex_translate
 
@@ -16,8 +17,16 @@ project_group = "import"
 inner = "terminology-0.9.0-1-rosa2014.1.x86_64.rpm"
 yandex_api_key = "trnsl.1.1.20160131T164826Z.1cd5efb8cc6af7a6.0d34545e70be2a8bdd261d6cf743ae3df1429d13"
 
+assert yandex_translate(yandex_api_key, "en-ru",
+                        "Lazy cat jumps over talking dog") == "Ленивый кот перепрыгивает через говорящая собака"
 
-print(str([full_project_info(project_group, f) for f in filter_input( inner)]))
+project_info = [full_project_info(project_group, f) for f in filter_input(inner)]
 
-assert yandex_translate(yandex_api_key, "en-ru", "Lazy cat jumps over talking dog") == "Ленивый кот перепрыгивает через говорящая собака"
+for f in project_info[0][3]:
+    print(str(f[0]))
+    for i in f[1]:
+        pair = i.split('=')
+        print(str(pair[0] + "=" + pair[1]))
+        print(str(pair[0] + "[ru]=" + yandex_translate(yandex_api_key, "en-ru", pair[1])))
 
+commit_patch(project_info[0][2][1], project_info[0][1], "TRANSLATION")
--- src/gitworks.py
+++ src/gitworks.py
@@ -1,14 +1,17 @@
+import codecs
 import uuid
-from subprocess import call
+from subprocess import call, PIPE
 
-random_str = uuid.uuid4().hex.capitalize()
-package_name = "terminology"
-call("cd /tmp/ && git clone https://abf.io/import/terminology.git " + random_str, shell=True)
 
-call("ls /tmp/" + random_str, shell=True)
-
-call("touch /tmp/" + random_str + "/" + random_str + ".patch", shell=True)
-
-call("sed -i \"1iPatch: " + random_str + ".patch\" /tmp/" + random_str + "/" + package_name + ".spec", shell=True)
-
-call("cd /tmp/" + random_str + " && git commit -am \"Переведено\"", shell=True)
+def commit_patch(repo_path, package_name, patch_content):
+    random_str = uuid.uuid4().hex.capitalize()
+    print(random_str)
+    call("cd /tmp/ && git clone " + repo_path + " " + random_str, shell=True, stdout=PIPE, stderr=PIPE)
+    call("ls /tmp/" + random_str, shell=True, stdout=PIPE, stderr=PIPE)
+    call("touch /tmp/" + random_str + "/" + random_str + ".patch", shell=True, stdout=PIPE, stderr=PIPE)
+    # here writes patch_content to ^^^^^
+    with codecs.open("/tmp/" + random_str + "/" + random_str + ".patch", "w", "utf-8-sig") as temp:
+        temp.write(patch_content + "\n")
+    call("sed -i \"1iPatch: " + random_str + ".patch\" /tmp/" + random_str + "/" + package_name + ".spec", shell=True,
+         stdout=PIPE, stderr=PIPE)
+    call("cd /tmp/" + random_str + " && git commit -am \"Переведено\"", shell=True, stdout=PIPE, stderr=PIPE)
