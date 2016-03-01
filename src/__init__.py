f493fd114e59148359be3b41fff53df4fc14b3b1
Dmitriy Yakovlev <zimy@yandex.ru>
2016-03-01 07:44:56 +0300

    Добавлены JSON-команды, передаваемые модулю __init__.py Убиться. Дожили.

--- src/__init__.py
+++ src/__init__.py
@@ -1,35 +1,40 @@
-# inner = sys.stdin.read()
 import json
 
-from gitworks import commit_patch
 from handsome import full_project_info
-from list_utils import filter_input
+from list_utils import filter_input, from_file_with_list
+from settings_keeper import load_settings, save_settings
 from translation import yandex_translate
 
-# 3 из репозитория.
-# TZ 4.1.3
-# Про архитектуру: модули и инструкции по расширению каждого модуля.
-
-
 project_group = "import"
-inner = "terminology-0.9.0-1-rosa2014.1.x86_64.rpm"
+inner = "/home/zimy/Documents/HotProjects/RosaLinuxLocalizer/src/terminology-0.9.0-1-rosa2014.1.x86_64.rpm"
 yandex_api_key = "trnsl.1.1.20160131T164826Z.1cd5efb8cc6af7a6.0d34545e70be2a8bdd261d6cf743ae3df1429d13"
 
-assert yandex_translate(yandex_api_key, "en-ru",
-                        "Lazy cat jumps over talking dog") == "Ленивый кот перепрыгивает через говорящая собака"
+# assert yandex_translate(yandex_api_key, "en-ru",
+#                         "Lazy cat jumps over talking dog") == "Ленивый кот перепрыгивает через говорящая собака"
 
-project_info = [full_project_info(project_group, f, ["Name", "Comment"]) for f in filter_input(inner)]
-print(json.dumps(project_info))
-translated_files = ""
+import argparse
 
-for one in project_info:
-    for f in one["desktop_files"]:
-        print(str(f["path"]))
-        translated_files = translated_files + f["path"] + "\n"
-        for i in f["strings"]:
-            translated = str(
-                i["variable_name"] + "[translated_ru]=" + yandex_translate(yandex_api_key, "en-ru", i["value"]["en"]))
-            translated_files = translated_files + translated + "\n"
-            print(translated)
+parser = argparse.ArgumentParser(description='Process some json.')
+parser.add_argument('json_data', metavar='JSON', type=str, help='JSON')
 
-commit_patch(project_info[0]["git"], project_info[0]["package_name"], translated_files)
+args = parser.parse_args()
+
+input_user = json.loads(str(args.json_data))
+
+if input_user["command"] == "translate":
+    print(yandex_translate(yandex_api_key, "en-ru", input_user["data"]))
+elif input_user["command"] == "get_settings":
+    print(load_settings())
+elif input_user["command"] == "set_settings":
+    save_settings(input_user["data"])
+    print("{\"result\":\"ok\"}")
+elif input_user["command"] == "import":
+    data = input_user["data"]
+    files = []
+    if data["type"] == "files":
+        files = filter_input(data["values"])
+    elif data["type"] == "custom":
+        files = from_file_with_list(data["values"])
+    project_info = [full_project_info(project_group, f, ["Name", "Comment"]) for f in files]
+    print(json.dumps(project_info))
+    # commit_patch(project_info[0]["git"], project_info[0]["package_name"], translated_files)
