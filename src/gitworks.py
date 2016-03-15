ecd1ffc95a5a4877ab9595e7262eb1cd5710367d
Dmitriy Yakovlev <zimy@yandex.ru>
2016-03-15 14:52:58 +0300

    git and presentation

--- src/gitworks.py
+++ src/gitworks.py
@@ -1,7 +1,7 @@
 import codecs
 import json
 import uuid
-from subprocess import call, PIPE
+from subprocess import call
 
 from settings_keeper import load_settings
 
@@ -26,9 +26,11 @@ def commit_patch(repo_path, package_name, patch_content, branch_name):
     call("ls /tmp/" + random_str, shell=True)
     call("touch /tmp/" + random_str + "/" + random_str + ".patch", shell=True)
     with codecs.open("/tmp/" + random_str + "/" + random_str + ".patch", "w", "utf-8-sig") as temp:
-        temp.write(patch_content + "\n")
+        for file in json.loads(patch_content):
+            temp.write("+++" + file["path"] + "\n" + "@@ -0,0 +1,3 @@" + "\n")
+            for line in file["strings"]:
+                temp.write(line["variable_name"] + "[ru]=" + line["value"]["ru"] + "\n")
 
     call("cd /tmp/" + random_str + "/ && git add " + random_str + ".patch", shell=True)
-    call("cd /tmp/" + random_str + "/ && git diff HEAD > " + random_str + ".patch", shell=True)
     call("sed -i \"1iPatch: " + random_str + ".patch\" /tmp/" + random_str + "/" + package_name + ".spec", shell=True)
     call("cd /tmp/" + random_str + " && git commit -am \"Переведено\" && git push", shell=True)
