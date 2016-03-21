e02a1dd0346a1adc456ea30d6eefb0ac477c1984
Dmitriy Yakovlev <zimy@yandex.ru>
2016-03-21 06:11:32 +0300

    Поправил работу с настройками, она стала более лучше

--- src/settings_keeper.py
+++ src/settings_keeper.py
@@ -1,12 +1,37 @@
 from os import path
+import json
+
+file_name = path.expanduser('~') + "/.config/handsome.json"
 
 
 def load_settings():
-    with open(path.expanduser('~')+"/.config/handsome.json", "r") as storage:
-        return storage.read()
+    if not path.isfile(file_name):
+        with open(file_name, "w") as storage:
+            storage.write("")
+        return json.dumps({
+            "yandex_api_key": "not set",
+            "abf_projects_group": "import",
+            "abf_login": "login",
+            "abf_password": "password",
+            "branches": [
+            ],
+            "variables": [
+                {
+                    "name": "Name",
+                    "last": False
+                },
+                {
+                    "name": "Comment",
+                    "last": True
+                }
+            ]
+        })
+    else:
+        with open(file_name, "r") as storage:
+            return storage.read()
 
 
 def save_settings(settings):
-    with open(path.expanduser('~')+"/.config/handsome.json", "w") as storage:
+    with open(file_name, "w") as storage:
         storage.write(settings)
     return settings
