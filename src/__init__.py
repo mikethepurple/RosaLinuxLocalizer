1d989b19da06af23c947326fa51e8b6ad8b590a3
Dmitriy Yakovlev <zimy@yandex.ru>
2016-03-22 05:35:03 +0300

    Arguments

--- src/__init__.py
+++ src/__init__.py
@@ -7,18 +7,14 @@ from list_utils import filter_input
 from translation import translate
 import uuid
 
-# 3 из репозитория.
-# TZ 4.1.3
-# Про архитектуру: модули и инструкции по расширению каждого модуля.
-
 import argparse
 
 parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='an integer for the accumulator')
-parser.add_argument('--sum', dest='accumulate', action='store_const',
-                    const=sum, default=max,
-                    help='sum the integers (default: find the max)')
+parser.add_argument('--git-branch')
+parser.add_argument('--with-manual-check')
+parser.add_argument('--makeall')
+parser.add_argument('--empty-strings')
+
 
 args = parser.parse_args()
 print(args.accumulate(args.integers))
